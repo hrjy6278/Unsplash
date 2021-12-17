4d9723ca1e2adf4e6b7874fa279e2780098fead7
hrjy6278 <hrjy6278@naver.com>
2021-12-17 23:55:58 +0900

    feat: Decoding을 위한 PasingManager 구조체 구현

--- Unsplash/Unsplash/NeworkService/PasingManager.swift
+++ Unsplash/Unsplash/NeworkService/PasingManager.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct PasingManager {
+    
+    enum PasingError: Error {
+        case decodeError
+    }
+    
+    static func decode<T: Decodable>(type: T.Type, data: Data) throws -> T {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(type, from: data)
+            return decodedData
+        } catch {
+            throw PasingError.decodeError
+        }
+    }
+}
