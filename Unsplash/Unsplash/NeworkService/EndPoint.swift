c4e01493ab0ce62724d7aa9ecb6bcfe83bf4dd2b
hrjy6278 <hrjy6278@naver.com>
2021-12-18 00:01:47 +0900

    feat: 사진 검색 API EndPoint 열거형 구현

---
+++ Unsplash/Unsplash/NeworkService/EndPoint.swift
@@ -0,0 +1,21 @@
+//
+//  EndPoint.swift
+//  Unsplash
+//
+//  Created by KimJaeYoun on 2021/12/17.
+//
+
+import Foundation
+
+enum EndPoint {
+    private static var BaseURL = "https://api.unsplash.com/"
+    
+    case searchPhotos
+    
+    var description: String {
+        switch self {
+        case .searchPhotos:
+            return Self.BaseURL + "search/photos"
+        }
+    }
+}
