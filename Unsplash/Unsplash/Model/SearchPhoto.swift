9c51abee1c7d04862ec024b923a799cfecb140ad
hrjy6278 <hrjy6278@naver.com>
2021-12-19 15:33:44 +0900

    refactor: Search Photo의 Nested 타입이였던 Photo 타입을 Nested 타입 해제
    - 재사용성이 있다고 판단하여 중첩타입이 아닌 일반 타입으로 변경하였다.

--- Unsplash/Unsplash/Model/SearchPhoto.swift
+++ Unsplash/Unsplash/Model/SearchPhoto.swift
@@ -17,36 +17,34 @@ struct SeachPhoto: Codable {
         case Photos = "results"
         case totalPages = "total_pages"
     }
+}
+
+struct Photo: Codable {
+    let id: String
+    let createdAt: String
+    let description: String?
+    let urls: Urls
+    let links: PhotoLink
     
-    struct Photo: Codable {
-        let id: String
-        let createdAt: Date
-        let description: String
-        let urls: Urls
-        let links: PhotoLink
-        
-        enum CodingKeys: String, CodingKey {
-            case id, description, urls, links
-            case createdAt = "created_at"
-        }
-    }
-    
-    struct PhotoLink: Codable {
-        let linksSelf, html, download : String
-        
-        enum CodingKeys: String, CodingKey {
-            case html, download
-            case linksSelf = "self"
-        }
-    }
-    
-    struct Urls: Codable {
-        let raw, full, regular, small, thumb: String
-    }
-    
-    struct ProfileImage: Codable {
-        let small, medium, large: String
+    enum CodingKeys: String, CodingKey {
+        case id, description, urls, links
+        case createdAt = "created_at"
     }
 }
 
+struct PhotoLink: Codable {
+    let linksSelf, html, download : String
+    
+    enum CodingKeys: String, CodingKey {
+        case html, download
+        case linksSelf = "self"
+    }
+}
 
+struct Urls: Codable {
+    let raw, full, regular, small, thumb: String
+}
+
+struct ProfileImage: Codable {
+    let small, medium, large: String
+}
