152f7a9e37474c1333e09b54f2de2428363b6988
hrjy6278 <hrjy6278@naver.com>
2021-12-19 11:11:13 +0900

    feat: CellID 하드코딩을 피하기 위한 Extension 구현

---
+++ Unsplash/Unsplash/Controller/SearchTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchTableViewCell.swift
+//  Unsplash
+//
+//  Created by KimJaeYoun on 2021/12/19.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
