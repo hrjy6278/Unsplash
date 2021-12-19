5c871b08ed7f933f56959c432086b65aa63abfd4
hrjy6278 <hrjy6278@naver.com>
2021-12-19 12:20:23 +0900

    feat: TabBarController 에 searchViewController 추가
    - SearchViewController 인스턴스 생성 및 tabImage 추가 하는 configure() 메서드 구현

--- Unsplash/Unsplash/Controller/UnsplashTabbarController.swift
+++ Unsplash/Unsplash/Controller/UnsplashTabbarController.swift
@@ -11,7 +11,22 @@ class UnsplashTabbarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Unsplash"
+        configure()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension UnsplashTabbarController {
+    private func configure() {
+        let searchViewController = SearchViewController()
+        let searchImage = UIImage(systemName: "magnifyingglass.circle")
+        let searchTapImage = UIImage(systemName: "magnifyingglass.circle.fill")
+        searchViewController.tabBarItem = UITabBarItem(title: "Search",
+                                                       image: searchImage,
+                                                       selectedImage: searchTapImage)
+    
+        viewControllers = [
+            searchViewController
+        ]
     }
 }
