1cd24f4b901317dfc2487b8466d2b3279a1f937c
hrjy6278 <hrjy6278@naver.com>
2021-12-19 11:05:58 +0900

    feat: 검색 결과 화면을 보여줄 TableView 생성 및 레이아웃 설정

---
+++ Unsplash/Unsplash/Controller/SearchViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Unsplash
+//
+//  Created by KimJaeYoun on 2021/12/17.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    //MARK: - Properties
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    //MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+    }
+}
+
+//MARK: - Method
+extension SearchViewController {
+    func setupLayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
