85ed92a69dd0a0802865a7a720ab80d4ce8d9af2
hrjy6278 <hrjy6278@naver.com>
2021-12-20 11:17:44 +0900

    refactor: 기존 NetworkService 구조체타입 클래스로 변경
    - 무한스크롤을 위해 다운로드 중인지 아닌지 판단하기 위한 프로퍼티 isFetching 선언
    - Static func 였던 searchPhotos 메서드를 인스턴스 메서드로 변경
    - 검색결과를 가져오는중인지 아닌지 판단하는 로직 추가

--- Unsplash/Unsplash/NeworkService/NetworkService.swift
+++ Unsplash/Unsplash/NeworkService/NetworkService.swift
@@ -8,19 +8,25 @@
 import Foundation
 import Alamofire
 
-struct NetworkService {
+final class NetworkService {
+    var isFetching = false
     
-    static func searchPhotos<T: Decodable>(type: T.Type,
-                                           query: String,
-                                           page: Int,
-                                           completion: @escaping (Result<T, Error>) -> Void) {
+    func searchPhotos<T: Decodable>(type: T.Type,
+                                    query: String,
+                                    page: Int,
+                                    completion: @escaping (Result<T, Error>) -> Void) {
+        
+        guard isFetching == false else { return }
+        
         let queryParameter: [String: String] = [
             "client_id": "UUGSJm9EzJM5tw1ngfNkKSn_OGW26g-O-z5FgLJEPPk",
             "page": String(page),
             "query": query
         ]
         
+        self.isFetching = true
         AF.request(EndPoint.searchPhotos.description, method: .get, parameters: queryParameter, encoder: .urlEncodedForm).responseData { responseData in
+            self.isFetching = false
             switch responseData.result {
             case .success(let data):
                 do {
