da171b88a6e4b50fd8d9020b91ca60582a3a7edc
hrjy6278 <hrjy6278@naver.com>
2021-12-22 20:40:07 +0900

    feat: 헤더뷰에 올려질 profileImageView, nameLabel 프로퍼티 선언

--- Unsplash/Unsplash/Controller/Profile/View/ProfileHeaderView.swift
+++ Unsplash/Unsplash/Controller/Profile/View/ProfileHeaderView.swift
@@ -8,5 +8,23 @@
 import UIKit
 
 class ProfileHeaderView: UITableViewHeaderFooterView {
-
+    //MARK: Properties
+    static let identifier = "ProfileHeaderView"
+    
+    private var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    private var nameLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .title2)
+        
+        return label
+    }()
 }
