07c1789dbe416aee3de4f24cf603973eda2fb55e
hrjy6278 <hrjy6278@naver.com>
2021-12-22 20:46:40 +0900

    feat: 프로필 정보를 담을 StackView 생성, StackView의 Layout, AddSubView 구현

--- Unsplash/Unsplash/Controller/Profile/View/ProfileHeaderView.swift
+++ Unsplash/Unsplash/Controller/Profile/View/ProfileHeaderView.swift
@@ -27,4 +27,34 @@ class ProfileHeaderView: UITableViewHeaderFooterView {
         
         return label
     }()
+    
+    private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
+        
+        return stackView
+    }()
+}
+
+//MARK: - Setup View And Layout
+extension ProfileHeaderView: HierarchySetupable {
+    func setupViewHierarchy() {
+        stackView.addArrangedSubview(profileImageView)
+        stackView.addArrangedSubview(nameLabel)
+        
+        contentView.addSubview(stackView)
+    }
+    
+    func setupLayout() {
+        let stackViewLeadingConstant: CGFloat = 16
+        
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leftAnchor,
+                                               constant: stackViewLeadingConstant)
+        ])
+    }
 }
