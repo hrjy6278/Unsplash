87a4205782028ec3c5b2f382a8bdeee326f0da12
hrjy6278 <hrjy6278@naver.com>
2021-12-22 16:56:49 +0900

    feat: 프로필 화면을 보여줄 ProfileViewController 생성

---
+++ Unsplash/Unsplash/Controller/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Unsplash
+//
+//  Created by KimJaeYoun on 2021/12/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
