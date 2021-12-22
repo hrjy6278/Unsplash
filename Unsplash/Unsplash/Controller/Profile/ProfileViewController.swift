e03f02737312eda5458584d6d2b187143bef0454
hrjy6278 <hrjy6278@naver.com>
2021-12-22 21:07:10 +0900

    feat: ProfileViewController에 tableView 프로퍼티 선언 및 Cell, HeaderView Register 등록

--- Unsplash/Unsplash/Controller/Profile/ProfileViewController.swift
+++ Unsplash/Unsplash/Controller/Profile/ProfileViewController.swift
@@ -13,8 +13,37 @@ class ProfileViewController: UIViewController, TabBarImageInfo {
     var selected = "person.fill"
     var barTitle = "Profile"
     
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(ProfileHeaderView.self,
+                           forHeaderFooterViewReuseIdentifier: ProfileHeaderView.identifier)
+        tableView.register(ImageListTableViewCell.self,
+                           forCellReuseIdentifier: ImageListTableViewCell.cellID)
+        tableView.sectionHeaderHeight = 88
+        return tableView
+    }()
+    
     //MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupView()
+    }
+}
+
+//MARK: - Setup View And Layout
+extension ProfileViewController: HierarchySetupable {
+    func setupViewHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    func setupLayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
