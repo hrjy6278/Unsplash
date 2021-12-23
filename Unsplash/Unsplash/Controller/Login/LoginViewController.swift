ebd069ead0d8dd05508d540d913b60c3251e8841
hrjy6278 <hrjy6278@naver.com>
2021-12-23 21:38:00 +0900

    feat: 로그인 버튼, 설명 라벨View 생성
    - 버튼의 addTarget으로 Oauth2ViewController 를 불러오도록 하였다.

--- Unsplash/Unsplash/Controller/Login/LoginViewController.swift
+++ Unsplash/Unsplash/Controller/Login/LoginViewController.swift
@@ -8,11 +8,62 @@
 import UIKit
 
 class LoginViewController: UIViewController, TabBarImageInfo {
+    //MARK: Properties
     var nomal = "person"
     var selected = "person.fill"
     var barTitle = "Profile"
     
+    private var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .callout)
+        label.text = "로그인이 필요합니다."
+        
+        return label
+    }()
+    
+    private var loginButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Login", for: .normal)
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+        button.addTarget(self, action: #selector(didTapLoginButton(_:)), for: .touchUpInside)
+        button.layer.cornerRadius = 5
+        button.backgroundColor = .gray
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupView()
+    }
+}
+
+//MARK: - Setup View And Layout
+extension LoginViewController: HierarchySetupable {
+    func setupViewHierarchy() {
+        view.addSubview(descriptionLabel)
+        view.addSubview(loginButton)
+    }
+    
+    func setupLayout() {
+        let descriptionLabelConstrant: CGFloat = -40
+        NSLayoutConstraint.activate([
+            descriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            descriptionLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor,
+                                                      constant: descriptionLabelConstrant),
+            
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+//MARK: - Login Button Action
+extension LoginViewController {
+    @objc func didTapLoginButton(_ sender: UIButton) {
+        navigationController?.pushViewController(Oauth2ViewController(), animated: true)
     }
 }
